cf403fe28ae007f4a7aa7132323b1455c5d5b6c9
Ray Seikel <rseikel@bigpond.com>
2021-03-23 11:02:19 +1100

    remove local dcloaders dependency

--- src/ssv/__init__.py
+++ src/ssv/__init__.py
@@ -1,7 +1,8 @@
 # flake8: noqa
 from . import ssvloaders
 from . import marz
-import dc_loaders as loaders
+from . import marzJSON
+#import dc_loaders as loaders
 from .plotting import plot_spectra
 from .helpers import (
     specutils_spectra_to_table_spectra, specutils_spectrum_to_table_spectrum,
